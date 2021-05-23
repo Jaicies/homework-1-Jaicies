9f9013c633cb0eafbe3e4d55daf5c3e368ca13e1
jaicies <smallwoodj19@students.ecu.edu>
2021-05-23 14:13:56 -0400

    I am having an issue where my GeoQuiz is running instead of pirate places, and I am looking for guidance in general to see if I am on the right track and where to go from here.

--- app/src/main/java/edu/ecu/cs/pirateplaces/PiratePlacesActivity.kt
+++ app/src/main/java/edu/ecu/cs/pirateplaces/PiratePlacesActivity.kt
@@ -2,11 +2,55 @@ package edu.ecu.cs.pirateplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class PiratePlacesActivity : AppCompatActivity() {
 
+    private lateinit var previousButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+
+    private val Places = listOf (
+        Place(R.string.College_Hill),
+        Place(R.string.West_End),
+        Place(R.string.Dowdy_Ficklen),
+        Place(R.string.Joyner))
+
+    private var currentIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_pirate_places)
+
+        previousButton = findViewById(R.id.previous_button)
+        nextButton = findViewById(R.id.next_button)
+
+        previousButton.setOnClickListener {
+            currentIndex = (currentIndex - 1)
+            updateQuestion()
+        }
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1)
+        if (currentIndex == Places.size) {
+               Toast.makeText(
+                   this,
+                R.string.last_place_toast,
+               Toast.LENGTH_SHORT)
+                   .show()
+            }
+
+           else {
+                updateQuestion()
+            }
+
+        }
+       updateQuestion()
+    }
+    private fun updateQuestion () {
+        val questionTextResId = Places[currentIndex].textResid
+        questionTextView.setText(questionTextResId)
     }
 }
